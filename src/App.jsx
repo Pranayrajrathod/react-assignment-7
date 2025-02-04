b8c0ca004d014dd98255920e80a439035d585652
Pranayrajrathod <pranayraj904@gmail.com>
2025-02-04 22:35:03 +0530

    first commit on Starting a new assignment on router

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-dark text-white text-center' style={{ minHeight:'100vh'}}>
+        <h1>raja ram mohan roy</h1>
+    </div>
+  )
+}
+
+export default App
